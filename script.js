356f8ebbdd1f031dadcbee083c1cd318ca9b4dac
Ciaran Lagan <clagan16@hotmail.com>
2023-02-07 15:25:40 +0000

    Ski-Dreams

---
+++ script.js
@@ -0,0 +1,48 @@
+gsap.from("#m1", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 100,
+})
+
+gsap.from("#m2", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
+
+gsap.from("#m3", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 200,
+})
+
+gsap.from("#m4", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 150,
+})
+
+gsap.from("#tr", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 150,
+})
+
+gsap.from("#crs", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 100,
+})
+
+gsap.from("#house", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 10,
+})
